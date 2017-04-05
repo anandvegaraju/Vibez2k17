beb9f27dae19b1aec271221df6849541a9711e49
anandvegaraju <v.anand.vamsi@gmail.com>
2017-04-06 01:06:10 +0530

    day3 final commit by anand

--- app/src/main/java/com/example/anand/vibez2k17/MainMenu.java
+++ app/src/main/java/com/example/anand/vibez2k17/MainMenu.java
@@ -3,8 +3,11 @@ package com.example.anand.vibez2k17;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +22,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainMenu extends AppCompatActivity {
     String phonenumber,name;
+    Button creditsbutton, contactusbutton;
     ViewPager viewPager;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,6 +34,16 @@ public class MainMenu extends AppCompatActivity {
         viewPager.setAdapter(viewPagerAdapter);
         Intent intent = getIntent();
         phonenumber = intent.getStringExtra("phonenumber");
+
+        creditsbutton = (Button)findViewById(R.id.button2);
+        creditsbutton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(),"Anand Vegaraju\nBharat VK",Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
 
     }
 }
