d1828c6bb7af56ba6f4dc57f3e708ab69fa8999a
anandvegaraju <v.anand.vamsi@gmail.com>
2017-04-08 01:21:18 +0530

    day5 first commit by anand

--- app/src/main/java/com/example/anand/vibez2k17/EventMenuActivity.java
+++ app/src/main/java/com/example/anand/vibez2k17/EventMenuActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageButton;
 
 /**
@@ -29,6 +30,49 @@ public class EventMenuActivity extends AppCompatActivity {
         technicalbutton = (ImageButton)findViewById(R.id.tech_button);
         sportsbutton = (ImageButton)findViewById(R.id.sportscat_button);
         intercolgbutton = (ImageButton)findViewById(R.id.intercolg_button);
+
+        culturalbutton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent gotocultural = new Intent(EventMenuActivity.this,CulturalEventsActivity.class);
+                        gotocultural.putExtra("phonenumber",phonenumber);
+                        startActivity(gotocultural);
+                    }
+                }
+        );
+
+        technicalbutton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent gototechnical = new Intent(EventMenuActivity.this,TechnicalEventsActivity.class);
+                        gototechnical.putExtra("phonenumber",phonenumber);
+                        startActivity(gototechnical);
+                    }
+                }
+        );
+
+        intercolgbutton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent gotointercolg = new Intent(EventMenuActivity.this,IntercollegeEventsActivity.class);
+                        gotointercolg.putExtra("phonenumber",phonenumber);
+                        startActivity(gotointercolg);
+                    }
+                }
+        );
+
+        sportsbutton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent gotosports = new Intent(EventMenuActivity.this,SportEventsActivity.class);
+                        gotosports.putExtra("phonenumber",phonenumber);
+                    }
+                }
+        );
 
 
 
---
+++ app/src/main/java/com/example/anand/vibez2k17/TechnicalEventsActivity.java
@@ -0,0 +1,24 @@
+package com.example.anand.vibez2k17;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Anand on 08-04-2017.
+ */
+
+public class TechnicalEventsActivity extends AppCompatActivity {
+    String phonenumber;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_technical);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+        Intent i = getIntent();
+        phonenumber = i.getStringExtra("phonenumber");
+    }
+}
