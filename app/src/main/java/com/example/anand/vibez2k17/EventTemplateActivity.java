b997503760a004c459f221fa768c2bbb82b4c134
anandvegaraju <v.anand.vamsi@gmail.com>
2017-04-08 20:14:58 +0530

    day6 2nd commit by anand

---
+++ app/src/main/java/com/example/anand/vibez2k17/EventTemplateActivity.java
@@ -0,0 +1,17 @@
+package com.example.anand.vibez2k17;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Anand on 08-04-2017.
+ */
+
+public class EventTemplateActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_template);
+    }
+}
